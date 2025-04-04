6433b257367c2375bea82480c17607353a6a67e6
George Michael <61968966+George-Michael-Dagogo@users.noreply.github.com>
2025-04-04 02:40:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ options.add_argument("--disable-popup-blocking")
 options.add_argument('--headless=new')  # Using headless mode
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')  # Important for headless on Windows
+options.add_argument('--disable-gpu')  
+options.add_argument('--incognito') # Important for headless on Windows
 
 # These are critical for headless mode to work properly
 options.add_argument('--window-size=1920,1080')  
